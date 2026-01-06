82a6ce48a37997b145a452a0b2dc637138113cc2
Shaheer Arif <shaheer.arif@ucalgary.ca>
2026-01-05 19:58:00 -0700

    Deploy site build

--- README.md
+++
@@ -1,1 +0,0 @@
-# shaheer-m-arif.github.io
--- src/components/PageTitle.jsx
+++
@@ -1,9 +0,0 @@
-import TypewriterTitle from "./TypewriterTitle";
-
-export default function PageTitle({ text, sizePt = 7 }) {
-  return (
-    <h2 style={{ fontSize: `calc(1em + ${sizePt}pt)` }}>
-      <TypewriterTitle text={text} />
-    </h2>
-  );
-}
